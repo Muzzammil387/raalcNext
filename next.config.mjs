e3ed06f53037c53c0b50e35cca7f617c41bfe012
zain sheikh <zaynsheikh881@gmail.com>
2024-07-26 18:50:40 +0500

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['en', 'ar', 'ch', 'ru'],
-    defaultLocale: 'en',
-  },
+  // i18n: {
+  //   locales: ['en', 'ar', 'ch', 'ru'],
+  //   defaultLocale: 'en',
+  // },
   images: {
     unoptimized: true,
   },
