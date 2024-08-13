7a2a842e958257cfb4416f1533ee347ddf8c0c4d
Muzammil <muzammil.sidtechno@gmail.com>
2024-08-13 11:02:08 +0500

    update

--- app/[lang]/gallery/page.js
+++ app/[lang]/gallery/page.js
@@ -1,37 +1,37 @@
 
 import React from 'react'
 import GalleryBox from './GalleryBox';
-import axios from 'axios';
-import config from "../../services/config.json";
+// import axios from 'axios';
+// import config from "../../services/config.json";
 
 
-export async function generateMetadata({ params, searchParams }, parent) {
-    const { lang, slug } = params;
-    try {
-      const response = await axios.get(`${config.apiEndPoint}webContents/gallery/${lang}`);
-      const data = response.data;
-      return {
-          title: data?.data?.meta_tag || "Raalc gallery",
-          description: data?.data?.meta_description || "Raalc gallery",
-      };
-    } catch (error) {
-      console.error('Error fetching product data:', error);
-      return {
-        title: 'Raalc', // fallback title in case of an error
-      };
-    }
-  }
+// export async function generateMetadata({ params, searchParams }, parent) {
+//     const { lang, slug } = params;
+//     try {
+//       const response = await axios.get(`${config.apiEndPoint}webContents/gallery/${lang}`);
+//       const data = response.data;
+//       return {
+//           title: data?.data?.meta_tag || "Raalc gallery",
+//           description: data?.data?.meta_description || "Raalc gallery",
+//       };
+//     } catch (error) {
+//       console.error('Error fetching product data:', error);
+//       return {
+//         title: 'Raalc', // fallback title in case of an error
+//       };
+//     }
+//   }
 
 
-export async function generateStaticParams() {
-    // Define all possible language codes
-    const languages = ['en', 'ar', 'ch', 'ru'];
+// export async function generateStaticParams() {
+//     // Define all possible language codes
+//     const languages = ['en', 'ar', 'ch', 'ru'];
     
-    // Generate the paths for each language
-    const paths = languages.map((lang) => ({ lang }));
+//     // Generate the paths for each language
+//     const paths = languages.map((lang) => ({ lang }));
     
-    return paths;
-  }
+//     return paths;
+//   }
 
 
 
