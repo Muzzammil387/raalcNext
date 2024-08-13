bb54fc3e87113a9035d0e9cbc3d380e467e2d3d0
Muzammil <muzammil.sidtechno@gmail.com>
2024-08-13 10:50:28 +0500

    update

--- app/[lang]/gallery/page.js
+++ app/[lang]/gallery/page.js
@@ -1,26 +1,26 @@
 
 import React from 'react'
 import GalleryBox from './GalleryBox';
-import axios from 'axios';
+// import axios from 'axios';
 import config from "../../services/config.json";
 
 
-export async function generateMetadata({ params, searchParams }, parent) {
-    const { lang, slug } = params;
-    try {
-      const response = await axios.get(`${config.apiEndPoint}webContents/gallery/${lang}`);
-      const data = response.data;
-      return {
-          title: data?.data?.meta_tag || "Raalc gallery",
-          description: data?.data?.meta_description || "Raalc gallery",
-      };
-    } catch (error) {
-      console.error('Error fetching product data:', error);
-      return {
-        title: 'Raalc', // fallback title in case of an error
-      };
-    }
-  }
+// export async function generateMetadata({ params, searchParams }, parent) {
+//     const { lang, slug } = params;
+//     try {
+//       const response = await axios.get(`${config.apiEndPoint}webContents/gallery/${lang}`);
+//       const data = response.data;
+//       return {
+//           title: data?.data?.meta_tag || "Raalc gallery",
+//           description: data?.data?.meta_description || "Raalc gallery",
+//       };
+//     } catch (error) {
+//       console.error('Error fetching product data:', error);
+//       return {
+//         title: 'Raalc', // fallback title in case of an error
+//       };
+//     }
+//   }
 
 
 export async function generateStaticParams() {
