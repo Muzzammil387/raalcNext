ddecf7129c9c9503b83b54c1e8d31291811e4419
Muzammil React(Developer) <79418859+muzammil7912@users.noreply.github.com>
2024-11-14 12:12:00 +0500

    ypdate

--- components/home/EventSlider.jsx
+++ components/home/EventSlider.jsx
@@ -56,11 +56,10 @@ const EventSlider = ({events,home}) => {
                 height={0}
                 alt=""
               />
-            </Link>
-
             <span className="absolute z-[1] left-0 right-0 mx-auto top-[50%] transform translate-y-[-50%] text-center uppercase text-white font-bold text-[1.25rem] w-[80%] leading-[1]">
               {title}
             </span>
+            </Link>
           </div>
 
           <div className="overlay absolute top-11 left-[2px] w-[99%] h-3/4 bg-[rgba(0, 0, 0, 0.5) rounded-[5rem]"></div>
