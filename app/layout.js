4152c26e6ff1ff9fafcd3c4965fab8e91d1864ea
Muzammil <muzammil.sidtechno@gmail.com>
2025-01-09 13:24:58 +0500

    mm

--- app/layout.js
+++ app/layout.js
@@ -9,35 +9,72 @@ import "../public/css/whatsapp.css";
 import "react-toastify/dist/ReactToastify.css";
 import Main from "./[lang]/main";
 import Script from "next/script";
-import { GoogleAnalytics } from '@next/third-parties/google'
-
-// import RedirectComponent from './RedirectComponent'; // Adjust path if needed
+import { GoogleAnalytics,GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Raalc",
   description: "RAALC’s journey began in 2013, when it was co-founded in the Emirate of Sharjah. Our success continued through years and we opened our two new branches in Ras Al Khaimah",
-  // robots: {
-  //   index: false,
-  // },
   verification: {
     google: "kvKmamV1GmbVp8AZbTRautEsWCs1-84lLD5oB5rjj4g"
   }
 };
 
 export default function RootLayout({ children }) {
-  
   return (
     <html lang="en">
-    
-      <body className={inter.className}>
-        <GoogleAnalytics gaId="GTM-NK2H57S" />
-        <GoogleAnalytics gaId="GTM-NK2H57S" />
-        <GoogleAnalytics gaId="G-4VM2WNCYK1" />
-          <Head>
+      <Head>
+        {/* Google Tag Manager (GTM-NK2H57S) */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NK2H57S');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+        {/* Google Analytics (G-4VM2WNCYK1) */}
+        <Script
+          id="ga"
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-4VM2WNCYK1`}
+        />
+        <Script
+          id="ga-config"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4VM2WNCYK1');
+            `,
+          }}
+        />
+        {/* End Google Analytics */}
 
       </Head>
+      <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NK2H57S"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager */}
+
+        <GoogleTagManager gaId="GTM-NK2H57S" /> 
+        <GoogleAnalytics gaId="G-4VM2WNCYK1" /> 
+
         <Main>{children}</Main>
       </body>
     </html>
