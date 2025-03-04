9ae2de3e7bcfd0368c53df133017dd1b9afc30fc
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2025-03-04 17:14:05 +0400

    second page

--- app/[lang]/real-estate/components/Header.js
+++ app/[lang]/real-estate/components/Header.js
@@ -14,7 +14,15 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
-import { raalc_white_logo, white_whatsapp_icon, home_page_bg_image, right_arrow } from '@/app/untils/imgimport';
+import { 
+    raalc_white_logo, 
+    white_whatsapp_icon, 
+    home_page_bg_image, 
+    right_arrow,
+    tridition,
+    trust,
+    history_main_image
+} from '@/app/untils/imgimport';
 import { Grid, Paper } from "@mui/material";
 import ForwardIcon from '@mui/icons-material/Forward';
 import HubSpotForm from './HubSpotForm';
@@ -32,6 +40,13 @@ const pages = [
 
 const Header = ({ lang }) => {
   const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const scrollToHomePage = () => {
+    const homePageSection = document.getElementById("home_page");
+    if (homePageSection) {
+      homePageSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -145,7 +160,7 @@ const Header = ({ lang }) => {
         </Toolbar>
       </Container>
     </AppBar>
-    <div>
+    <div id='home_page'>
     <div className={styles.background_image_style}>
       {/* Background Image */}
       <div
@@ -213,6 +228,113 @@ const Header = ({ lang }) => {
       </div>
     </div>
     </div>
+
+    <div>
+        <div className={styles.history_container}>
+            <h2 className={styles.history_heading}>
+            Founded in 2013, RAALC has grown into a powerhouse of legal excellence, serving clients across the UAE with a
+            commitment to innovation and strategic legal solutions.
+            </h2>
+            <Grid container spacing={0} style={{display: "flex", alignItems: "center"}}>
+          <Grid item xs={12} md={5}>
+          <div style={{height: 600, marginTop: 30}}>
+                <Image 
+            src={history_main_image ?? ""} 
+            width={1000} 
+            height={1000} 
+            style={{height: "100%"}}
+            alt="About Us Page Main Image" 
+            />
+                </div>
+          </Grid>
+
+          <Grid item xs={12} md={7} className={styles.about_us_content}>
+          <div style={{marginTop: 40}}>
+            <p className={styles.ensure_protection} style={{display: "flex"}}>
+                <div >
+                <Image 
+            src={tridition ?? ""} 
+            width={250} 
+            height={100} 
+            alt="Tradition of Excellence Logo" 
+            />
+                </div>
+<div style={{marginLeft: 15}}>
+<p className={styles.bold}>
+            Tradition of Excellence
+            </p>
+            With a strong foundation in real estate, corporate, and financial law, we provide expert representation and strategic counsel to safeguard our clients’ interests.
+</div>
+            </p>
+          </div>
+          <div style={{marginTop: 25}}>
+          <p className={styles.ensure_protection_trust} style={{display: "flex"}}>
+                <div>
+                <Image 
+            src={trust ?? ""} 
+            width={300} 
+            height={200} 
+            alt="Technology Logo" 
+            />
+                </div>
+<div style={{marginLeft: 15}}>
+<p className={styles.bold}>
+            Technology-Driven Legal Services
+            </p>
+            RAALC is shaping the future of law by integrating AI and blockchain solutions into our practice, making us one of the UAE’s pioneers in smart-law innovation.
+            </div>
+            </p>
+          </div>
+          <div style={{marginTop: 25}}>
+          <p className={styles.ensure_protection_trust} style={{display: "flex"}}>
+                <div>
+                <Image 
+            src={trust ?? ""} 
+            width={300} 
+            height={200} 
+            alt="Trust Logo" 
+            />
+                </div>
+<div style={{marginLeft: 15}}>
+<p className={styles.bold}>
+            Your Trusted Legal Partner
+            </p>
+            From litigation to advisory, we craft tailored legal strategies that protect your rights and investments. Experience excellence—partner with RAALC today.
+            </div>
+            </p>
+          </div>
+          </Grid>
+        
+        </Grid>
+
+        <div>
+        <div className={styles.section_container}>
+      <span className={styles.section_text}>Speak to a Real Estate Lawyer Now</span>
+      <div className={styles.section_line}></div>
+      <Button  onClick={scrollToHomePage} className={styles.get_started_btn}>
+        GET STARTED
+      </Button>
+    </div>
+        </div>
+{/* 
+        <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: 30}}>
+        <h1 className={styles.about_us_footer}>
+        Speak to a Real Estate Lawyer Now
+        </h1>
+        
+        <Button
+    //   onClick={onClick}
+      variant="contained"
+      className={styles.get_started_btn}
+    >
+      GET STARTED
+    </Button>
+    
+    </div> */}
+
+        </div>
+    </div>
+
     </div>
 
   );
