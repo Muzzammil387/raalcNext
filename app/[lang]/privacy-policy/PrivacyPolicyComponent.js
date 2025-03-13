d94d349cf78d2d887ad65d361b2239a411b4b843
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2025-03-13 09:15:04 +0400

    add missing

--- app/[lang]/privacy-policy/PrivacyPolicyComponent.js
+++ app/[lang]/privacy-policy/PrivacyPolicyComponent.js
@@ -31,7 +31,23 @@ const PrivacyPolicyComponent = ({ lang }) => {
       <p className={styles.paragraph}>
         This page is used to inform website visitors regarding our policies with
         the collection, use, and disclosure of Personal Information if anyone
-        decides to use our Service, the www.raalc.ae website.
+        decides to use our Service, &nbsp;
+        <Link href="https://www.raalc.ae/" target="_blank">
+          <span className={styles.hyper_link}>www.raalc.ae</span>
+        </Link>
+        &nbsp; website.
+      </p>
+
+      <p className={styles.paragraph}>
+      If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.
+      </p>
+     <p className={styles.paragraph}>
+      The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible through&nbsp;
+      <Link 
+      href="https://www.raalc.ae/terms-and-conditions/" target="_blank">
+       <span className={styles.hyper_link}>Terms and Conditions Page</span> 
+    </Link> 
+      &nbsp;, unless otherwise defined in this Privacy Policy.
       </p>
 
       <div className={styles.sub_heading_container}>
@@ -138,6 +154,39 @@ const PrivacyPolicyComponent = ({ lang }) => {
               to review the Privacy Policy of these websites. We have no control over
               and assume no responsibility for the content, privacy policies, or
               practices of any third-party sites or services.
+            </p>
+          </ListItem>
+        </List>
+      </div>
+
+      <div className={styles.sub_heading_container}>
+      <h3 className={styles.sub_heading}>7. CHILDREN&apos;S PRIVACY</h3>
+        <List sx={{ listStyleType: "lower-alpha", pl: 4 }}>
+          <ListItem sx={{ display: "list-item" }}>
+            <p className={styles.paragraph}>
+            Our Services do not address anyone under the age of 13. We do not knowingly collect personal identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.
+            </p>
+          </ListItem>
+        </List>
+      </div>
+
+      <div className={styles.sub_heading_container}>
+      <h3 className={styles.sub_heading}>8. CHANGES TO THIS PRIVACY POILICY</h3>
+        <List sx={{ listStyleType: "lower-alpha", pl: 4 }}>
+          <ListItem sx={{ display: "list-item" }}>
+            <p className={styles.paragraph}>
+            We may update our Privacy Policy from time to time. Thus, we advise you to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately, after they are posted on this page.
+            </p>
+          </ListItem>
+        </List>
+      </div>
+
+      <div className={styles.sub_heading_container}>
+      <h3 className={styles.sub_heading}>9. CONTACT US</h3>
+        <List sx={{ listStyleType: "lower-alpha", pl: 4 }}>
+          <ListItem sx={{ display: "list-item" }}>
+            <p className={styles.paragraph}>
+            If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.
             </p>
           </ListItem>
         </List>
