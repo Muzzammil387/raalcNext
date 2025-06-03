b9f4836597ebecb7d1b7c3548b0f287bc59bd7fe
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2025-06-03 14:51:51 +0400

    fix hreflang

--- app/[lang]/SeoTags.jsx
+++ app/[lang]/SeoTags.jsx
@@ -4,13 +4,14 @@ import { usePathname } from 'next/navigation';
 
 export default function SeoTags({ baseUrl, currentLang }) {
   const pathname = usePathname();
+   const cleanPath = pathname.replace(/^\/(en|ar|ru|ch)(\/|$)/, '/');
 
   return (
     <>
       <link rel="canonical" href={`${baseUrl}${pathname}`} />
-      <link rel="alternate" hreflang="en" href={`${baseUrl}/en${pathname}`} />
-      <link rel="alternate" hreflang="ar" href={`${baseUrl}/ar${pathname}`} />
-      <link rel="alternate" hreflang="x-default" href={`${baseUrl}${pathname}`} />
+      <link rel="alternate" hreflang="en" href={`${baseUrl}/en${cleanPath}`} />
+      <link rel="alternate" hreflang="ar" href={`${baseUrl}/ar${cleanPath}`} />
+      <link rel="alternate" hreflang="x-default" href={`${baseUrl}${cleanPath}`} />
     </>
   );
 }
