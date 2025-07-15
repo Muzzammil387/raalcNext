29e89a564fc32efcc6004440ac0b5042514da641
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2025-07-15 12:39:30 +0400

    add vlaidation on new fields

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -27,7 +27,7 @@ export async function POST(req) {
   } = formData;
 
   // Basic validation example
-  if (!name || !email || !phone || !message || !assignedAdmin) {
+  if (!name || !email || !phone || !message || !assignedAdmin || !consultation_mode || !time) {
     return NextResponse.json(
       { ok: false, message: 'Missing required fields or assignedAdmin.' },
       { status: 400 }
